0b51bea0bc53aa6a1f2aa2ea8150133f16813f08
DevSw <64429051+devsw-prayas@users.noreply.github.com>
2026-08-14 01:28:49 +0530

    Add: Added helpers and non temporal forms for memcpy, win32 Valloc and Vfree wrappers

--- src/Private/stl_arrays.cpp
+++ src/Private/stl_arrays.cpp
@@ -1,7 +1,11 @@
 #include "stl_arrays.h"
 
-namespace Micro::STL::Arrays {
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <Windows.h>
+#include <immintrin.h>
 
+namespace Micro::STL::Arrays {
 	// Requires a multiple of 128 size, and assumes that it is aligned
 	void memcpy(void* __restrict src, void* __restrict dst, Bytes len) noexcept {
 		// Guarantees that this will always be called with 32B aligned memory
@@ -29,7 +33,7 @@ namespace Micro::STL::Arrays {
 	}
 
 	// Only requires len to be multiple of 128
-	void memcpyu(void* __restrict src, void* __restrict dst, Bytes len) noexcept{
+	void memcpyu(void* __restrict src, void* __restrict dst, Bytes len) noexcept {
 		// Use AVX2, unroll at 128 bytes per cycle
 		auto srcB = static_cast<std::byte*>(src);
 		auto dstB = static_cast<std::byte*>(dst);
@@ -48,29 +52,6 @@ namespace Micro::STL::Arrays {
 			srcB += 128;
 			dstB += 128;
 		}
-	}
-
-	void memcopy(void* src, void* dst, size_t len){
-		// Perform alignment and tail checks
-		size_t tail = len & 127;
-		if((reinterpret_cast<uintptr_t>(src) & 31) == 0 
-		&& (reinterpret_cast<uintptr_t>(dst) & 31) == 0 )
-			if (len > (64ull * 1024)) memcpyStream(src, dst, len);
-			else 
-			// Fully aligned, let 'em rip
-			memcpy(src, dst, len - tail);
-		else {
-			if (len > (64ull * 1024)) memcpyuStream(src, dst, len);
-			else
-			// No alignment guarantees
-			memcpyu(src, dst, len- tail);
-		}
-
-		// Now finish tail
-		auto* s = static_cast<std::byte*>(src) + (len - tail);
-		auto* d = static_cast<std::byte*>(dst) + (len - tail);
-
-		for(size_t i = 0; i < tail; i++) d[i] = s[i];
 	}
 
 	void memcpyStream(void* src, void* dst, Bytes len) noexcept {
@@ -120,16 +101,39 @@ namespace Micro::STL::Arrays {
 		_mm_sfence();
 	}
 
-	void memrevcpy(void* __restrict src, void* __restrict dst, Bytes len) noexcept{
+	void memcopy(void* src, void* dst, size_t len) {
+		// Perform alignment and tail checks
+		size_t tail = len & 127;
+		if ((reinterpret_cast<uintptr_t>(src) & 31) == 0
+			&& (reinterpret_cast<uintptr_t>(dst) & 31) == 0)
+			if (len > (64ull * 1024)) memcpyStream(src, dst, len);
+			else
+				// Fully aligned, let 'em rip
+				memcpy(src, dst, len - tail);
+		else {
+			if (len > (64ull * 1024)) memcpyuStream(src, dst, len - tail);
+			else
+				// No alignment guarantees
+				memcpyu(src, dst, len - tail);
+		}
+
+		// Now finish tail
+		auto* s = static_cast<std::byte*>(src) + (len - tail);
+		auto* d = static_cast<std::byte*>(dst) + (len - tail);
+
+		for (size_t i = 0; i < tail; i++) d[i] = s[i];
+	}
+
+	void memrevcpy(void* __restrict src, void* __restrict dst, Bytes len) noexcept {
 		// Guarantees that this will always be called with 32B aligned memory
 		__assume((reinterpret_cast<uintptr_t>(src) & 31) == 0);
 		__assume((reinterpret_cast<uintptr_t>(dst) & 31) == 0);
 
 		// Use AVX2, unroll at 128 bytes per cycle
 		auto srcB = static_cast<std::byte*>(src) + len;
-		auto dstB = static_cast<std::byte*>(dst) + len;		
+		auto dstB = static_cast<std::byte*>(dst) + len;
 		__m256i a, b, c, d;
-		while (len){
+		while (len) {
 			srcB -= 128;
 			dstB -= 128;
 
@@ -142,17 +146,17 @@ namespace Micro::STL::Arrays {
 			_mm256_store_si256(reinterpret_cast<__m256i*>(dstB + 32), b);
 			_mm256_store_si256(reinterpret_cast<__m256i*>(dstB + 64), c);
 			_mm256_store_si256(reinterpret_cast<__m256i*>(dstB + 96), d);
-			
+
 			len -= 128;
 		}
 	}
 
-	void memrevcpyu(void* __restrict src, void* __restrict dst, Bytes len) noexcept{
+	void memrevcpyu(void* __restrict src, void* __restrict dst, Bytes len) noexcept {
 		// Use AVX2, unroll at 128 bytes per cycle
 		auto srcB = static_cast<std::byte*>(src) + len;
-		auto dstB = static_cast<std::byte*>(dst) + len;		
+		auto dstB = static_cast<std::byte*>(dst) + len;
 		__m256i a, b, c, d;
-		while (len){
+		while (len) {
 			srcB -= 128;
 			dstB -= 128;
 
@@ -165,35 +169,108 @@ namespace Micro::STL::Arrays {
 			_mm256_storeu_si256(reinterpret_cast<__m256i*>(dstB + 32), b);
 			_mm256_storeu_si256(reinterpret_cast<__m256i*>(dstB + 64), c);
 			_mm256_storeu_si256(reinterpret_cast<__m256i*>(dstB + 96), d);
-			
+
 			len -= 128;
 		}
 	}
 
-	void memrevcopy(void* src, void* dst, size_t len){
+	void memrevcopy(void* src, void* dst, size_t len) {
 		// Perform alignment and tail checks
 		size_t tail = len & 127;
-		if((reinterpret_cast<uintptr_t>(src) & 31) == 0 
-		&& (reinterpret_cast<uintptr_t>(dst) & 31) == 0 )
+		if ((reinterpret_cast<uintptr_t>(src) & 31) == 0
+			&& (reinterpret_cast<uintptr_t>(dst) & 31) == 0)
 			// Fully aligned, let 'em rip
 			memrevcpy(src, dst, len - tail);
 		else {
 			// No alignment guarantees
-			memrevcpyu(src, dst, len- tail);
+			memrevcpyu(src, dst, len - tail);
 		}
 
 		// Now finish tail
 		auto* s = static_cast<std::byte*>(src);
 		auto* d = static_cast<std::byte*>(dst);
-		if(!tail) return;
+		if (!tail) return;
 		for (size_t i = tail; i-- > 0; ) d[i] = s[i];
 	}
 
 	void memmove(void* __restrict src, void* __restrict dst, Bytes len) noexcept {
 		auto* s = static_cast<std::byte*>(src);
 		auto* d = static_cast<std::byte*>(dst);
-		
-		if(d < s || d >= s+ len) memcopy(src, dst, len);
+
+		if (d < s || d >= s + len) memcopy(src, dst, len);
 		else memrevcopy(src, dst, len);
+	}
+
+	namespace Memory {
+		bool MemHandle::isValid() const {
+			if (!m_BaseAddress) return false;
+			return true;
+		}
+
+		bool VirtualMemory::alloc(MemHandle* handle, size_t size, MemOp operation) {
+			if (!handle) return false;
+
+			switch (operation) {
+			case MemOp::Reserve:
+				if (handle->isValid()) return false;
+				{
+					auto v = reserveAlign(size);
+					handle->m_BaseAddress = VirtualAlloc(nullptr, v, MEM_RESERVE, PAGE_NOACCESS);
+					if (!handle->m_BaseAddress) return false;
+					handle->m_CommittedSize = 0;
+					handle->m_TotalSize = v;
+					return true;
+				}
+			case MemOp::Commit:
+				if (handle->isValid()) {
+					if (handle->m_CommittedSize > handle->m_TotalSize)
+						return false;
+					if (size > SIZE_MAX - (PAGE_SIZE - 1))
+						return false;
+
+					auto v = alignToPage(size);
+					if (v > handle->m_TotalSize - handle->m_CommittedSize)
+						return false;
+
+					auto nPtr = std::bit_cast<void*>(std::bit_cast<uintptr_t>(handle->m_BaseAddress) + handle->m_CommittedSize);
+					if (!VirtualAlloc(nPtr, v, MEM_COMMIT, PAGE_READWRITE)) return false;
+					handle->m_CommittedSize += v;
+					return true;
+				}
+				return false;
+			case MemOp::Decommit:
+			case MemOp::Release:  break;
+			}
+			__assume(false);
+		}
+
+		bool VirtualMemory::free(MemHandle* handle, size_t size, MemOp operation) {
+			if (!handle) return false;
+
+			switch (operation) {
+			case MemOp::Decommit:
+				if (handle->isValid()) {
+					auto v = alignToPage(size);
+					if (v > handle->m_CommittedSize) return false;
+					auto nPtr = static_cast<std::byte*>(handle->m_BaseAddress)
+						+ handle->m_CommittedSize - v;
+					if (!VirtualFree(nPtr, v, MEM_DECOMMIT)) return false;
+					handle->m_CommittedSize -= v;
+					return true;
+				}
+				return false;
+			case MemOp::Release:
+				if (handle->isValid()) {
+					if (!VirtualFree(handle->m_BaseAddress, 0, MEM_RELEASE)) return false;
+					handle->m_BaseAddress = nullptr;
+					handle->m_CommittedSize = handle->m_TotalSize = 0;
+					return true;
+				}
+				return false;
+			case MemOp::Commit:
+			case MemOp::Reserve: break;
+			}
+			__assume(false);
+		}
 	}
 }
--- src/Public/stl_arrays.h
+++ src/Public/stl_arrays.h
@@ -37,11 +37,9 @@ name& operator=(const name&) = default;
 #endif
 
 // includes
-
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
-#include <Windows.h>
-#include <immintrin.h>
+#include <bit>
+#include <cstddef>
+#include <cstdint>
 #include <utility>
 #include <type_traits>
 
@@ -50,63 +48,81 @@ namespace Micro::STL::Arrays {
 	using Bytes = size_t;
 	using Pvoid = void*;
 
+	static constexpr Bytes PAGE_SIZE = 4ULL * 1024;
+	static constexpr Bytes RESERVE_ALIGNMENT = 64ULL * 1024;
+	static constexpr Bytes PAGE_ALIGNMENT = 4 * 1024ULL;
+
+	// Alignment
+	FORCEINLINE size_t reserveAlign(size_t v) {
+		return (v + RESERVE_ALIGNMENT - 1) & ~(RESERVE_ALIGNMENT - 1);
+	}
+
+	FORCEINLINE size_t alignToPage(size_t v) {
+		return (v + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1);
+	}
+
+	FORCEINLINE size_t align(size_t v, size_t align) {
+		return (v + align - 1) & ~(align - 1);
+	}
+
+	FORCEINLINE void* alignPtr(void* p, size_t align) {
+		auto v = std::bit_cast<uintptr_t>(p);
+		return reinterpret_cast<void*>(align(v, align));
+	}
+
 	// Assumes best case inputs always, must be wrapped
 
 	void memcpy(void* __restrict src, void* __restrict dst, Bytes len) noexcept;
 	void memrevcpy(void* __restrict src, void* __restrict dst, Bytes len) noexcept;
 
+	// Streaming / non temporal version
+	void memcpyStream(void* __restrict src, void* __restrict dst, Bytes len) noexcept;
+
 	bool memcmp(void* __restrict src, void* __restrict dst, Bytes len) noexcept;
 	void memset(void* src, std::byte val) noexcept;
 
-	// Unaligned support versions, upto the wrapper to choose which to call	
+	// Unaligned support versions, upto the wrapper to choose which to call
 
 	void memcpyu(void* __restrict src, void* __restrict dst, Bytes len) noexcept;
 	void memrevcpyu(void* __restrict src, void* __restrict dst, Bytes len) noexcept;
 
+	// Streaming / non temporal version
+	void memcpyuStream(void* __restrict src, void* __restrict dst, Bytes len) noexcept;
+
 	bool memcmpu(void* __restrict src, void* __restrict dst, Bytes len) noexcept;
 	void memsetu(void* src, std::byte val) noexcept;
 
-	// Streaming / non temporal versions
-	void memcpyStream(void* __restrict src, void* __restrict dst, Bytes len) noexcept;
-	void memcpyuStream(void* __restrict src, void* __restrict dst, Bytes len) noexcept;
-	
-
 	// User Facing calls
-
 	void memcopy(void* src, void* dst, size_t len);
 	void memrevcopy(void* src, void* dst, size_t len);
 	void memmove(void* __restrict src, void* __restrict dst, Bytes len) noexcept;
 
-
-	template<typename T> 
-	void memoryCopy(T* src, T* dst, Bytes len){
+	template<typename T>
+	void memoryCopy(T* src, T* dst, Bytes len) {
 		if (!src || !dst) return;
 		static_assert(std::is_trivially_copyable_v<T> && std::is_trivially_move_constructible_v<T>,
-		"Invalid type, cannot perform byte copying");
+					  "Invalid type, cannot perform byte copying");
 		//Delegate to actual call
 		memcopy(src, dst, len);
 	}
 
 	template<typename T>
-	void memMove(T* src, T* dst, Bytes len){
+	void memMove(T* src, T* dst, Bytes len) {
 		if (!src || !dst) return;
 		static_assert(std::is_trivially_copyable_v<T> && std::is_trivially_move_constructible_v<T>,
-		"Invalid type, cannot perform byte copying");
+					  "Invalid type, cannot perform byte copying");
 		//Delegate to actual call
 		memmove(src, dst, len);
 	}
 
-	static constexpr Bytes PAGE_SIZE = 4ULL * 1024;
-	static constexpr Bytes PAGE_GRANULARITY = 64ULL * 1024;
-
 	Pvoid align(Pvoid p_Ptr, size_t v_Align);
 	Bytes align(Bytes v_Val, size_t v_Align);
 
-	namespace Memory {
+		namespace Memory {
 		struct ALIGNAS(32) MemHandle final {
-			Bytes m_TotalSize;
-			Bytes m_CommittedSize;
-			Pvoid m_BaseAddress;
+			Bytes m_TotalSize = 0;
+			Bytes m_CommittedSize = 0;
+			Pvoid m_BaseAddress = nullptr;
 
 		private:
 			MAYBE_UNUSED Bytes reserved_ = 0;
@@ -115,6 +131,21 @@ namespace Micro::STL::Arrays {
 				MOVE_DEFAULTS(MemHandle)
 				MemHandle() = default;
 			~MemHandle() = default;
+
+			bool isValid() const;
+		};
+
+		enum class MemOp : uint8_t {
+			Reserve,
+			Commit,
+			Decommit,
+			Release
+		};
+
+		class VirtualMemory final {
+		public:
+			static bool alloc(MemHandle* handle, size_t size, MemOp operation);
+			static bool free(MemHandle* handle, size_t size, MemOp operation);
 		};
 	}
 }
